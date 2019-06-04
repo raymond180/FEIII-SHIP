0dac025618e93f306294707a0bc17551daa7f583
Raymond Chen <raymond180@hotmail.com>
2019-06-04 22:49:17 +0800

    update

--- topicModeling/compute_lda.sh
+++ topicModeling/compute_lda.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=shipper_matching
 #SBATCH --output %j.output.txt
-#SBATCH --error %j.error.txt
+#SBATCH --error %j.output.txt
 #SBATCH --time=24:00:00
 #SBATCH --qos=batch
 #SBATCH --nodes=6
@@ -12,27 +12,27 @@
 
 source ~/miniconda3/bin/activate
 
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" harmonized_shipper_asym 10 &
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" harmonized_shipper_asym 20 &
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" harmonized_shipper_asym 30 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" harmonized_shipper_sym 10 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" harmonized_shipper_sym 20 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" harmonized_shipper_sym 30 &
 
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" shipper_harmonized_asym 10 &
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" shipper_harmonized_asym 20 &
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" shipper_harmonized_asym 30 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" shipper_harmonized_sym 10 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" shipper_harmonized_sym 20 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/compute_lda.py 1 "https://obj.umiacs.umd.edu/feiiiship/Data/Dataset/BofL6country.zip" shipper_harmonized_sym 30 &
 
 
 wait
 
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py harmonized_shipper_asym 10 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py harmonized_shipper_sym 10 &
 
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py shipper_harmonized_asym 10 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py shipper_harmonized_sym 10 &
 
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py harmonized_shipper_asym 20 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py harmonized_shipper_sym 20 &
 
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py shipper_harmonized_asym 20 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py shipper_harmonized_sym 20 &
 
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py harmonized_shipper_asym 30 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py harmonized_shipper_sym 30 &
 
-srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py shipper_harmonized_asym 30 &
+srun --nodes=1 --ntasks=1 --exclusive python ~/FEIII-SHIP/topicModeling/lda_analysis.py shipper_harmonized_sym 30 &
 
 wait
